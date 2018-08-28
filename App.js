7b3f1c8b81ff32c282c666f1f259a24fea1c66be
Robert Pirritano <rpirritano@gmail.com>
2018-08-28 16:33:48 -0700

    added background color as state

--- App.js
+++ App.js
@@ -1,29 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+//make the color be passed in state so user can change it
+  constructor() {
+    super()
+    this.state = {
+      backgroundColor: 'blue'
+    }
+  }
+
   render() {
+    //use ES6 destructuring
+    const { backgroundColor } = this.state
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={[styles.container, {backgroundColor}]}>
+  
       </View>
     );
   }
@@ -35,15 +29,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
