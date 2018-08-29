5ea282c71449f91210a0b3a932785def713c2cdc
Robert Pirritano <rpirritano@gmail.com>
2018-08-29 13:17:21 -0700

    added background color to color info via ColorTools

--- components/ColorInfo.js
+++ components/ColorInfo.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
-
+import ColorTools from 'color'
 //navigate.state.params.color is the second parameter of onSelect={() => navigate('Details', {color})}/>
 //in ColorList
-const ColorInfo = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text style={styles.text}>{ navigation.state.params.color }</Text>
-  </View>
-)
+const ColorInfo = ({ navigation }) => {
+  const color = ColorTools(navigation.state.params.color)
+  return (
+    <View style={[styles.container, { backgroundColor: color }]}>
+      <Text style={styles.text}>To Do: Display Details</Text>
+    </View>
+  )
+}
 
 ColorInfo.navigationOptions = ({ navigation }) => ({
   title: navigation.state.params.color
